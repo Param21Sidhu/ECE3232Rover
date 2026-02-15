c17527379c090b25770b127b88251ef2bfed9a38
Param21Sidhu <paramveersidhu21@gmail.com>
2026-02-15 14:15:40 -0400

    added 0501 new functions

--- Mainproject.c
+++ Mainproject.c
@@ -19,7 +19,6 @@
 
 //variables (storage registers)
 uint8_t TXMSG = 0; //declaring as Tx outgoing 8 bits to be placed on TX1REG for transmission
-int TXRDY = 0; //declaring TXRDY as the ready for transmission bit (there might be a cleaner way to implement)
 uint8_t RXMSG = 0; //declaring as Rx incoming 8 bits to be stored from RC1REG as received
 
 void __interrupt() ISR(){ //Interrupt handler
@@ -27,15 +26,12 @@ void __interrupt() ISR(){ //Interrupt handler
     if(PIR3bits.TXIF == 1){ //ready for another Transmission (Tx1REG is empty)
         
         TX1REG = TXMSG; //byte to be transmitted
-        PIR3bits.TXIF = 0; //clear Tx flag
-        TXRDY = 1; //Ready for next Transmission
         TXMSG = 0x00; //reset TXMSG so same message is not continuously sent
+        PIE3bits.TXIE = 0;
     }
     
     if(PIR3bits.RCIF == 1){ //Data on receiver (incoming Transmission)
-       
         RXMSG = RC1REG; //store received byte
-        PIR3bits.RCIF = 0; //clear Rx flag
     }
 }
 
@@ -43,20 +39,55 @@ void UARTSYNC(){ //sends Tx sync bits
     
     int x = 0; //sync sent counter
     
-    while(x < 2){ //loop until both sync sent
+        while(x < 2){ //loop until both sync sent
+            
+            if((PIR3bits.TXIF == 1) && (x == 0)){ //if ready for transmission and no other sync send, send first sync
+                TXMSG = 0xFE; //first sync code
+                PIE3bits.TXIE = 1;
+                x = 1; //ready for next sync
+            }
+            else if((PIR3bits.TXIF == 1) && (x == 1)){ //if ready for transmission and first sync sent, send second sync
+                TXMSG = 0x19; //second sync code
+                PIE3bits.TXIE = 1;
+                x = 2;
+            }
+        }
+}
+
+void GETPCUINFO(){ //sends Tx sync bits
     
-        if((TXRDY == 1) && (x == 0)){ //if ready for transmission and no other sync send, send first sync
-            TXMSG = 0xF3; //first sync code
-            TXRDY = 0; //unready TXMSG
-            x = 1; //ready for next sync
+    int x = 0; //sync sent counter
+    
+        while(x < 2){ //loop until both sync sent
+            
+            if((PIR3bits.TXIF == 1) && (x == 0)){ //if ready for transmission and no other sync send, send first sync
+                TXMSG = 0x01; //first sync code
+                PIE3bits.TXIE = 1;
+                x = 1; //ready for next sync
+            }
+            else if((PIR3bits.TXIF == 1) && (x == 1)){ //if ready for transmission and first sync sent, send second sync
+                TXMSG = 0x04; //second sync code
+                PIE3bits.TXIE = 1;
+                x = 2;
+            }
         }
-        else if((TXRDY == 1) && (x == 1)){ //if ready for transmission and first sync sent, send second sync
-            TXMSG = 0x19; //second sync code
-            TXRDY = 0; //unready sync
-            x = 2;
-        }
+}
+
+void SEND_0501_GET_INFO(void)
+{
+    // Send: FE 19 01 05 00 00
+    uint8_t bytes[6] = {0xFE, 0x19, 0x01, 0x05, 0x00, 0x00};
+
+    for (int i = 0; i < 6; i++) {
+        while (PIR3bits.TXIF == 0) { }
+        TXMSG = bytes[i];
+        PIE3bits.TXIE = 1;
+        while (PIE3bits.TXIE == 1) {
+        
+        } // wait ISR to send + turn it off
     }
 }
+
 
 void main(void) { //main function
     
@@ -70,13 +101,17 @@ void main(void) { //main function
     TX1STAbits.TXEN = 1; //Enable transmitter
     TX1STAbits.SYNC = 0; //Asynchronous mode
     RC1STAbits.SPEN = 1; //Receiver enabled
+    RC6PPS = 0x10; //sets Tx to port RC6 (port is output and digital by default)
    
     //interrupts setup
     INTCONbits.PEIE = 1; //Peripheral interrupts enabled 
     INTCONbits.GIE = 1; //Global interrupts enabled
-    PIE3bits.TXIE = 1; //USART Transmit interrupt enabled 
-    PIE3bits.RCIE = 1; //USART Receive interrupt enabled
-
+    //PIE3bits.TXIE = 1; //USART Transmit interrupt enabled 
+    //PIE3bits.RCIE = 1; //USART Receive interrupt enabled
+    
+//    UARTSYNC();
+//    GETPCUINFO();
+    SEND_0501_GET_INFO();
     while(1){ //always active loop
         //do something
     }
